6396b6577d28fa740553062eebe2f62600ecb488
Rafaela Mielli Bonacim <ra_bonacim@magazineluiza.intranet>
2020-11-18 14:30:54 -0300

    subindo o cod

---
+++ products/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer, SellerSerializer
+from .models import Seller, Product
+from django.http import JsonResponse
+
+class ProductViewSet(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailViewSet(generics.RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class SellerViewSet(generics.ListCreateAPIView):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+
+
+class SellerDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
